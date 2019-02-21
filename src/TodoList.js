e13c29ab042447a48107cbf3f8f57a2db38a7adb
1393925530 <zzc1393925530@gmail.com>
2019-02-21 21:31:10 +0800

    使用Redux实现input输入框实时显示

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,13 +8,21 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        store.subscribe(this.handleStoreChange);
     }
 
     render() {
         return (
         <div style={{marginTop: '10px',marginLeft: '10px'}}>
             <div>
-                <Input value = {this.state.inputValue} placeholder='todo info' style={{width : '300px',marginRight: '10px'}}/>
+                <Input 
+                value = {this.state.inputValue} 
+                placeholder='todo info' 
+                style={{width : '300px',marginRight: '10px'}}
+                onChange = {this.handleInputChange}
+                />
                 <Button type="primary">提交</Button>
             </div>
             <List
@@ -25,6 +33,19 @@ class TodoList extends Component {
             />
         </div>
         )
+    }
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value', //描述要做什么事情
+            value: e.target.value
+        }
+        store.dispatch(action);
+        console.log(e.target.value);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState());
+        console.log('store changed');
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,14 @@ const defaultState = {
     list: [1,2]
 };
 
+//reducer 可以接收state，但不能修改state
 export default (state = defaultState, action) => {
+    if (action.type === "change_input_value") {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    console.log(state,action);
     //state管理员需要的记录本的所有数据
     return state;
 }
