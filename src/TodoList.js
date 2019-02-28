8fdea1c9e99a16e6a0dab82f0dbf9ece786a4735
1393925530 <zzc1393925530@gmail.com>
2019-02-28 19:28:45 +0800

    react-redux完善TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class TodoList extends Component {
+const TodoList = (props) => {
 
-    render() {
-        return (
+    const { inputValue, list, changeInputValue, handleClick, handleDelete } = props;
+
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={this.props.inputValue} onChange={this.changeInputValue}/>
-                    <button>提交</button>
-                </div>
-                <ul>
-                    <li>dell</li>
-                </ul>
+                <input value={inputValue} onChange={changeInputValue} />
+                <button onClick={handleClick}>提交</button>
             </div>
-        )
-    }
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return <li onClick={handleDelete} key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 //state映射到props，state是store里面的数据，映射到组件的props
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     }
 }
 
@@ -34,8 +39,22 @@ const mapDispatchToProps = (dispatch) => {
                 value: e.target.value
             }
             dispatch(action);
+        },
+        handleClick() {
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action);
+        },
+        handleDelete(index) {
+            const action = {
+                type: 'delete_item',
+                index
+            }
+            dispatch(action);
         }
     }
 }
 
+//TodoList是UI组件，在经过connect将UI组件和业务逻辑相结合，实际导出的是容器组件
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
